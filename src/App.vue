9918dece0543c4a143a926815a233fbac46970bf
Myke Cameron <mykecameron@gmail.com>
2022-02-03 15:28:45 -0500

    pass some data into a component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Gratitude</h1>
-  <Gratitude/>
+  <Gratitude :prompts="prompts"/>
 </template>
 
 <script>
@@ -10,6 +10,42 @@ export default {
   name: 'App',
   components: {
     Gratitude
+  },
+  data() {
+    return {
+      prompts: [
+        {
+          text: 'What I focused on today',
+        },
+        {
+          text: 'Cute things I saw today',
+        },
+        {
+          text: 'New things I tried today',
+        },
+        {
+          text: 'Ways I supported others today',
+        },
+        {
+          text: 'Ways I loved myself today',
+        },
+        {
+          text: 'Fun things I did today',
+        },
+        {
+          text: 'Ways I supported others today',
+        },
+        {
+          text: 'Ways I challenged myself today',
+        },
+        {
+          text: 'What inspired me today',
+        },
+        {
+          text: 'What I let go of today',
+        },
+      ]
+    };
   }
 }
 </script>
