b1ec0bb6859c89f1ca63305b4b8cda957544c12f
Myke Cameron <mykecameron@gmail.com>
2022-02-03 13:18:29 -0500

    clean up hello world

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Gratitude</h1>
+  <Gratitude/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gratitude from './components/Gratitude.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Gratitude
   }
 }
 </script>
